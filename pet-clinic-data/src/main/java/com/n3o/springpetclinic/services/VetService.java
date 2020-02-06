8ce3385969340c280784dc627fbd0ae46413ead7
Nilesh767 <nilesh.choudhary797@gmail.com>
2020-02-06 20:40:07 +0530

    Added a single interface rather then multiple interfaces. Closes #11

--- pet-clinic-data/src/main/java/com/n3o/springpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/com/n3o/springpetclinic/services/VetService.java
@@ -2,10 +2,6 @@ package com.n3o.springpetclinic.services;
 
 import com.n3o.springpetclinic.model.Vet;
 
-import java.util.Set;
 
-public interface VetService {
-    Vet findById(Long id);
-    Vet save(Vet vet);
-    Set<Vet> findAll();
+public interface VetService extends CrudService<Vet, Long> {
 }
