4fb658d8372ab5c8cc3ec8ed4a6b2e3b7c210dfb
Nilesh767 <nilesh.choudhary797@gmail.com>
2020-02-08 20:45:35 +0530

    Refactored map implementation to abstract ID property. Closes #23

--- pet-clinic-web/src/main/java/com/n3o/springpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/n3o/springpetclinic/bootstrap/DataLoader.java
@@ -21,13 +21,11 @@ public class DataLoader implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         Owner owner1 = new Owner();
-        owner1.setId(1L);
         owner1.setFirstName("Nilesh");
         owner1.setLastName("Choudhary");
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner2.setId(2L);
         owner2.setFirstName("Rubal");
         owner2.setLastName("Choudhary");
         ownerService.save(owner2);
@@ -35,13 +33,11 @@ public class DataLoader implements CommandLineRunner {
         System.out.println("Owners Loaded");
 
         Vet vet1 = new Vet();
-        vet1.setId(1L);
         vet1.setFirstName("Sam");
         vet1.setLastName("Axe");
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet2.setId(2L);
         vet2.setFirstName("Jessie");
         vet2.setLastName("Porter");
         vetService.save(vet2);
