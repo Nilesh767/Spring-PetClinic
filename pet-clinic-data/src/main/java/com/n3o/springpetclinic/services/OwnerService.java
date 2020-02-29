1ca2e9cd52b5872669a1bd06aa4def1059639f13
Nilesh767 <nilesh.choudhary797@gmail.com>
2020-02-29 18:53:14 +0530

    implement find owner. closes #54

--- pet-clinic-data/src/main/java/com/n3o/springpetclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/com/n3o/springpetclinic/services/OwnerService.java
@@ -2,7 +2,11 @@ package com.n3o.springpetclinic.services;
 
 import com.n3o.springpetclinic.model.Owner;
 
+import java.util.List;
+
 
 public interface OwnerService extends CrudService<Owner, Long> {
     Owner findByLastName(String lastName);
+
+    List<Owner> findAllByLastNameLike(String lastName);
 }
